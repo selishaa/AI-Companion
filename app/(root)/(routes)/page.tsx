c5f4100e0f2de3f0cc4d6b950cd26b92fab6d13d
Selisha <selishathapa5@gmail.com>
2024-06-23 22:40:33 -0400

    Started the AI companion Project.

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+const RootPage = () => {
+    return (
+        <div>
+      <UserButton afterSignOutUrl="/" />
+        </div>
+    );
+    }
+    export default RootPage;
