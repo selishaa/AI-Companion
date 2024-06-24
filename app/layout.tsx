3eb6c85aff737d926440b2078caadf1bfccda257
Selisha <selishathapa5@gmail.com>
2024-06-24 04:39:35 -0400

    Added dark mode,navbar & sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,14 +16,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
    <ClerkProvider> 
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        {children}</body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+
+        </ThemeProvider>
+      
+        </body>
     </html>
    </ClerkProvider> 
   );
