5d0c593c79e41c5ab349c241a6a877ace989110c
Selisha <selishathapa5@gmail.com>
2024-06-28 23:35:45 -0400

    Worked on Chat UI

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,5 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import { CompanionForm } from "./components/companion-form";
+import { auth, redirectToSignIn } from "@clerk/nextjs/server";
+import { RedirectToSignIn } from "@clerk/nextjs";
 
 interface CompanionIdPageProps {
     params: {
@@ -10,11 +12,18 @@ interface CompanionIdPageProps {
 const CompanionIdPage = async({
     params
 }: CompanionIdPageProps) => {
+    const { userId } = auth ();
 //To do check subscription
+if (!userId) {
+    return redirectToSignIn ();
+}
+
+
 
 const companion = await prismadb.companion.findUnique({
     where: {
         id: params.companionId,
+        userId,
     },
 });
 
