7fead3671091903b85f88beb85b93767cfbd7d37
Selisha <selishathapa5@gmail.com>
2024-07-06 21:17:57 -0400

    STRIPE UI

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,6 +9,7 @@ import { ModeToggle } from "./mode-toggle";
 
 import { cn } from "@/lib/utils";
 import { MobileSidebar } from "./mobile-sidebar";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 
 const font = Poppins({
@@ -16,7 +17,15 @@ const font = Poppins({
     subsets:["latin"]
 });
 
-export const Navbar = () => {
+interface NavbarPro {
+    isPro: boolean;
+  };
+
+export const Navbar = ({ isPro }: NavbarPro) => {
+    const proModal = useProModal();
+
+
+
     return (
         <div className="fixed w-full z-50 flex justify-between items-center py-2
          px-4 border-b border-primary/10 bg-secondary h-16">
@@ -34,7 +43,7 @@ export const Navbar = () => {
 
             
             <div className="flex items-center gap-x-3">
-                <Button variant="premium" size="sm">
+                <Button onClick= {proModal.onOpen} variant="premium" size="sm">
                     Upgrade
                     <Sparkles className="h-4 w-4 fill-white text-white ml-2"/>
                 </Button>
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/api/webhook"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
